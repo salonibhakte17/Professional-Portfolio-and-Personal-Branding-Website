aa6f038417074e3206c7161d946003d5c0cfa55d
Saloni Bhakte <salonibhakte@gmail.com>
2026-07-02 15:55:05 +0530

    Create a GET /contact route that sends your phone number or email address as plain text.

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const path = require("path");
 
 const app = express();
 const PORT = 4000;
-
-// Serve HTML + CSS + JS from "public" folder
 app.use(express.static(path.join(__dirname, "public")));
-
-// Required route (must be plain text)
 app.get("/contact", (req, res) => {
   res.type("text/plain");
   res.send("salonibhakte@gmail.com");
